b6ecf5c3cf5854224fb8502cd1a6ac06f36515d4
Austin Stowe <austinrstowe@gmail.com>
2023-02-21 11:31:32 -0700

    Add project files.

---
+++ Mission08_0209/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Mission08_0209.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
